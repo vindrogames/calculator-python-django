5b124e2195cf1d9472a58a64f4fe78daaf9894ad
zeneke <zeneke@vindrogames.com>
2023-12-07 00:47:49 +0100

    last changes

--- app/main/settings.py
+++ app/main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="forlocaltest")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=1))
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ") if os.environ.get('ALLOWED_HOSTS') else ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ") if os.environ.get('ALLOWED_HOSTS') else ['localhost', '127.0.0.1','*']
 
 
 # Application definition
